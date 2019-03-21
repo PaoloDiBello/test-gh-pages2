8a3b79463aa8bd6c95350998a911140e0b9f98c2
PaoloDiBello <dibellopol@gmail.com>
2019-03-21 11:53:11 +0100

    Updates

---
+++ precache-manifest.436a0beaf2071200687faaf90a4b6824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f066488666af72d770f4",
+    "url": "/test-gh-pages2/static/js/runtime~main.448b6bf0.js"
+  },
+  {
+    "revision": "208d2ddd6e5d2654748a",
+    "url": "/test-gh-pages2/static/js/main.0fdc0c45.chunk.js"
+  },
+  {
+    "revision": "696e66af247ddc83d43e",
+    "url": "/test-gh-pages2/static/js/2.9121c4a9.chunk.js"
+  },
+  {
+    "revision": "208d2ddd6e5d2654748a",
+    "url": "/test-gh-pages2/static/css/main.3f340e52.chunk.css"
+  },
+  {
+    "revision": "175fc2f5f072dbe89607771a702dd1b7",
+    "url": "/test-gh-pages2/index.html"
+  }
+];
